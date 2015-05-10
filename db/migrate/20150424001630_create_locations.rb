93315e189366224d85308b533dc91f4d4b46b379
Matthew Villwock <mtvillwock@gmail.com>
2015-05-09 17:56:54 -0700

    removed address field from location migration because it is not necessary yet

--- db/migrate/20150424001630_create_locations.rb
+++ db/migrate/20150424001630_create_locations.rb
@@ -2,7 +2,6 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :city
-      t.string :address
       t.integer :locationable_id
       t.string :locationable_type
 
