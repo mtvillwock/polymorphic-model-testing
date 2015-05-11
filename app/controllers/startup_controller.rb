9434f127bdabf7eacefeb82581574045f923f47e
Matthew Villwock <mtvillwock@gmail.com>
2015-05-10 21:45:35 -0700

    remove debugging p statements, change else p statement to render json

---
+++ app/controllers/startup_controller.rb
@@ -0,0 +1,16 @@
+class StartupController < ApplicationController
+  def create
+    @startup = Startup.new(name: params[:name])
+    if @startup.save
+      @profile = Profile.new(description: params[:description],
+                               profileable_id: @startup.id,
+                               profileable_type: @startup.class.name).save
+      @location = Location.new(city: params[:city],
+                                locationable_id: @startup.id,
+                                locationable_type: @startup.class.name).save
+    else
+      render :json { error: "startup failed to save"}
+    end
+    redirect_to '/'
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
   def create
-    p params
-    p @startup = Startup.find_by(name: params[:startup_name])
-    p @user = User.new(name: params[:name], startup_id: @startup.id)
+    @startup = Startup.find_by(name: params[:startup_name])
+    @user = User.new(name: params[:name], startup_id: @startup.id)
     if @user.save
-      p @profile = Profile.new(description: params[:description],
+      @profile = Profile.new(description: params[:description],
                                profileable_id: @user.id,
                                profileable_type: @user.class.name).save
-      p @location = Location.new(city: params[:city],
+      @location = Location.new(city: params[:city],
                                 locationable_id: @user.id,
                                 locationable_type: @user.class.name).save
     else
-      p "user didn't save"
+      render :json { error: "startup failed to save"}
     end
     redirect_to '/'
   end
