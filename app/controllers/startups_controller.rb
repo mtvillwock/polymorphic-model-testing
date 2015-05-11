e5b460cd176a939237841959e2e3e6124b7fdc36
Matthew Villwock <mtvillwock@gmail.com>
2015-05-10 21:23:41 -0700

    got forms working, creating a user or startup creates an associated location and profile; note: you need to enter an existing startup in proper capitalization because there is no input sanitization currently

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,2 +1,17 @@
 class StartupsController < ApplicationController
+  def create
+    p params
+    @startup = Startup.new(name: params[:name])
+    if @startup.save
+      p @profile = Profile.new(description: params[:description],
+                               profileable_id: @startup.id,
+                               profileable_type: @startup.class.name).save
+      p @location = Location.new(city: params[:city],
+                                locationable_id: @startup.id,
+                                locationable_type: @startup.class.name).save
+    else
+      p "startup didn't save"
+    end
+    redirect_to '/'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
     p @startup = Startup.find_by(name: params[:startup_name])
     p @user = User.new(name: params[:name], startup_id: @startup.id)
     if @user.save
-      p @user.profile.new(description: params[:description],
+      p @profile = Profile.new(description: params[:description],
                                profileable_id: @user.id,
                                profileable_type: @user.class.name).save
-      p @user.location.new(description: params[:city],
+      p @location = Location.new(city: params[:city],
                                 locationable_id: @user.id,
                                 locationable_type: @user.class.name).save
     else
